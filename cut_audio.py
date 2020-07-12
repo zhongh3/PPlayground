339275b733eb3a12c7c9b3648bcc5305fe6a75ae
zhongh3 <zhon0017@gmail.com>
2020-07-12 22:04:17 +0800

    add metadata support, encoding choice

--- cut_audio.py
+++ cut_audio.py
@@ -3,13 +3,20 @@ import os
 import pandas as pd
 
 
-def parse_input(csv_file, in_file, out_dir, out_ext):
+def parse_input(csv_file, in_file, out_dir, out_ext, artist, album, re_encode=False):
     cmds = []
+    if re_encode:
+        codec = '-c:a libfdk_aac -b:a 256k -ar 44100 -ac 2 -vn'
+    else:
+        codec = '-c copy'
+
     df = pd.read_csv(csv_file)
     for row in df.values:
         track, out_name, st_hh, st_min, st_sec, et_hh, et_min, et_sec = row
         out_name = out_name.replace(':', ' ')  # handle invalid char in file name
-        cmds.append('ffmpeg -i {} -ss {:0>2d}:{:0>2d}:{:0>2d} -to {:0>2d}:{:0>2d}:{:0>2d} -c copy "{}.{}"'.format(
+        cmds.append('ffmpeg -i {} -ss {:0>2d}:{:0>2d}:{:0>2d} -to {:0>2d}:{:0>2d}:{:0>2d} {} '
+                    '-metadata track={} -metadata title="{}" -metadata album_artist="{}" '
+                    '-metadata album="{}" {}"{:0>2d} {}.{}"'.format(
             in_file,
             st_hh,
             st_min,
@@ -17,7 +24,14 @@ def parse_input(csv_file, in_file, out_dir, out_ext):
             et_hh,
             et_min,
             et_sec,
-            out_dir + out_name,
+            codec,
+            track,
+            out_name,
+            artist,
+            album,
+            out_dir,
+            track,
+            out_name,
             out_ext))
 
     return cmds
@@ -29,11 +43,15 @@ def main():
     in_file = './data/' + '2015.m4a'
     out_ext = 'm4a'
     out_dir = './data/'
-    cmds = parse_input(csv_file, in_file, out_dir, out_ext)
+    artist = '柴咲コウ'
+    album = '2015 Live'
+    re_encode = True
+    commands = parse_input(csv_file, in_file, out_dir, out_ext, artist, album, re_encode=re_encode)
 
-    for cmd in cmds:
-        subprocess.run(cmd, shell=True)
-    return cmds
+    for cmd in commands:
+        print(cmd)
+        # subprocess.run(cmd, shell=True)
+    return commands
 
 
 if __name__ == "__main__":
